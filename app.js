09ce4df3d22b696d028e3d3e14f29f084dec49b3
Yusuf Gözükara <101390306+yusufgozukara@users.noreply.github.com>
2022-06-06 13:39:26 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,60 @@
+let random = Math.floor(Math.random() * 100 +1) ;
+let input = document.querySelector(".input");
+let check = document.querySelector('.check');
+let restart = document.querySelector('.restart');
+let paragraf = document.querySelector('.para');
+let life = 7;
+
+
+
+const guessGame = function(){
+
+    if(life > 1){
+        if(input.value > 100 || input.value < 0){
+            paragraf.innerText = "Please enter a valid number";
+        } else{
+            if(input.value > random){
+                paragraf.innerHTML = 'Lower number please';
+                life--;
+                document.querySelector('.last').innerText = input.value;
+                dow.play();
+            } 
+
+            else if(input.value < random){
+                paragraf.innerHTML = 'Greater number please';
+                life--;
+                document.querySelector('.first').innerText = input.value;
+                up.play();
+
+            } else if(input.value == random){
+                paragraf.innerHTML = 'Woaaww. Are you a mindreader?'
+                life--;
+                check.style.display = 'none';
+                restart.style.display = 'block';
+                applause.play();
+
+                           
+            }
+        }
+    
+    } else {
+        paragraf.innerHTML = 'Game Over';
+        check.style.display = 'none';
+        input.style.display = 'none';
+        restart.style.display = 'block';
+        life--;
+        loser.play();
+    }
+    document.querySelector('.life-span').innerHTML = life;
+    input.value = "";
+    
+}
+
+check.addEventListener('click', guessGame);
+
+
+check.addEventListener('keydown', (e) => {
+    if(e.code == 'Enter'){
+        guessGame();
+    }
+})
